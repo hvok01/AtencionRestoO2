b7167fe6858a106862605677b4419bc4941fc5b5
Aldana1128 <leonela2811@gmail.com>
2018-10-22 22:49:22 -0300

    prueba

--- src/atencionresto/Reserva.java
+++ src/atencionresto/Reserva.java
@@ -5,10 +5,88 @@
  */
 package atencionresto;
 
+import java.time.LocalDate;
+
 /**
  *
- * @author Administrador
+ * @author nn
  */
 public class Reserva {
+    
+   private int idReserva;
+    private String nombreCliente;
+    private int dniCliente;
+    private LocalDate fechaHora;
+    private boolean estadoVigente;
+    private Mesa mesa;
+
+    public Reserva(int idReserva, String nombreCliente, int dniCliente, LocalDate fechaHora, boolean estadoVigente, Mesa mesa) {
+        this.idReserva = idReserva;
+        this.nombreCliente = nombreCliente;
+        this.dniCliente = dniCliente;
+        this.fechaHora = fechaHora;
+        this.estadoVigente = estadoVigente;
+        this.mesa = mesa;
+    }
+
+    public Reserva(String nombreCliente, int dniCliente, LocalDate fechaHora, boolean estado, Mesa mesa) {
+        this.nombreCliente = nombreCliente;
+        this.dniCliente = dniCliente;
+        this.fechaHora = fechaHora;
+        this.estadoVigente = estadoVigente;
+        this.mesa = mesa;
+    }
+
+    public Reserva() {
+    }
+    
+
+    public int getIdReserva() {
+        return idReserva;
+    }
+
+    public void setIdReserva(int idReserva) {
+        this.idReserva = idReserva;
+    }
+
+    public String getNombreCliente() {
+        return nombreCliente;
+    }
+
+    public void setNombreCliente(String nombreCliente) {
+        this.nombreCliente = nombreCliente;
+    }
+
+    public int getDniCliente() {
+        return dniCliente;
+    }
+
+    public void setDniCliente(int dniCliente) {
+        this.dniCliente = dniCliente;
+    }
+
+    public LocalDate getFechaHora() {
+        return fechaHora;
+    }
+
+    public void setFechaHora(LocalDate fechaHora) {
+        this.fechaHora = fechaHora;
+    }
+
+    public boolean getEstadoVigente() {
+        return estadoVigente;
+    }
+
+    public void setEstadoVigente(boolean estadoVigente) {
+        this.estadoVigente = estadoVigente;
+    }
+
+    public Mesa getMesa() {
+        return mesa;
+    }
+
+    public void setMesa(Mesa mesa) {
+        this.mesa = mesa;
+    }
     
 }
--- src/atencionresto/ReservaData.java
+++ src/atencionresto/ReservaData.java
@@ -5,10 +5,119 @@
  */
 package atencionresto;
 
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
- * @author Administrador
+ * @author nn
  */
 public class ReservaData {
+    private Connection connection = null;
+private Conexion conexion;
+private MesaData mesa;
+
+ 
+    public ReservaData(Conexion conexion) {
+        try {
+            connection = conexion.getConexion();
+        } catch (ClassNotFoundException ex) {
+        Logger.getLogger(ReservaData.class.getName()).log(Level.SEVERE, null, ex);
+    }
+    }
+
+   public void guardarReserva(Reserva reserva){
+        try {
+            
+            String sql = "INSERT INTO reserva (nombre_cliente, dni_cliente, fecha_hora, id_mesa, estado_vigente) VALUES ( ? , ? , ? , ? , ? );";
+
+            try (PreparedStatement statment = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+                statment.setString(1, reserva.getNombreCliente());
+                statment.setInt(2, reserva.getDniCliente());
+                statment.setDate(3, Date.valueOf(reserva.getFechaHora()));
+                statment.setBoolean(4, reserva.getEstadoVigente());
+                statment.setInt(5, reserva.getMesa().getIdMesa());
+  
+ 
+                statment.executeUpdate();
+                
+                ResultSet rs = statment.getGeneratedKeys();//id's
+                
+                if (rs.next()) {
+                    reserva.setIdReserva(rs.getInt(1));
+                } else {
+                    System.out.println("No se pudo obtener el id luego de insertar una reserva");}
+            }
+        } catch (SQLException ex) {
+            System.out.println("Error al insertar una reserva: " + ex.getMessage());
+        }
+    }
     
+        
+    public List<Reserva> obtenerReservas() throws ClassNotFoundException {
+        List<Reserva> reservas = new ArrayList<>();
+            
+        try {
+            String sql = "SELECT * FROM reserva;";
+            try (PreparedStatement statment = connection.prepareStatement(sql)) {
+                ResultSet resultSet = statment.executeQuery();
+                Reserva reserva;
+                while(resultSet.next()){
+                    reserva = new Reserva();
+                    conexion=new Conexion();
+                    conexion.getConexion();
+                    reserva.setIdReserva(resultSet.getInt(1));
+                    reserva.setNombreCliente(resultSet.getString(2));
+                    reserva.setDniCliente(resultSet.getInt(3));
+                    reserva.setFechaHora(resultSet.getDate(4).toLocalDate());
+                    reserva.setEstadoVigente(resultSet.getBoolean(5));
+                    statment.setInt(5, reserva.getMesa().getIdMesa());
+  
+              
+                    reservas.add(reserva);
+                }
+            }
+        } catch (SQLException ex) {
+            System.out.println("Error al obtener las reservas: " + ex.getMessage());
+        }catch(NullPointerException e){
+                System.out.println("Error3"+e.getLocalizedMessage());
+        }
+        
+        return reservas;
+    }
+  
+     
+     public void borrarReserva(int idReserva) throws ClassNotFoundException{
+
+        try {
+            String sql = "DELETE FROM reserva where id= ?;";
+          try (PreparedStatement statment = connection.prepareStatement(sql)) {
+              statment.setInt(1, idReserva);
+            }
+        } catch (SQLException ex) {
+            System.out.println("Error al borrar la reserva: " + ex.getMessage());
+        }
+    } 
+    
+     
+    public void cancelarReserva(int idReserva) throws SQLException{
+        
+       try {
+        String sql = "UPDATE FROM reserva SET estado_vigente=0 where id_reserva= ? ;";
+       try (PreparedStatement statment = connection.prepareStatement(sql)) {
+              statment.setInt(1, idReserva);
+            }  
+       } catch (SQLException ex) {
+            System.out.println("Error al cancelar la reserva: " + ex.getMessage());
+        }
+    }
 }
+
