0988ec75fa915eef3ffac3c563354ce60f1a26b0
Aldana1128 <leonela2811@gmail.com>
2018-10-22 22:58:29 -0300

    VistaMesa

--- src/home/VistaMesa.java
+++ src/home/VistaMesa.java
@@ -13,54 +13,43 @@ import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.table.DefaultTableModel;
-
+import Atencionresto.*;
+import atencionresto.Mesa;
 /**
  *
  * @author Usuario
  */
 public class VistaMesa extends javax.swing.JInternalFrame {
 DefaultTableModel modelo;
-private MesaData mesaData;
 private Conexion conexion;
-  
- 
-    public VistaMesa(){
+private ArrayList<Mesa> listaMesa; 
+private ArrayList<Mesa> listaMesaData; 
+private MesaData mesadata;
+private Mesa mesa;
+
+  public VistaMesa(){
         initComponents();
-        mostrardatos();
     try {
         conexion = new Conexion("jdbc:mysql://localhost/universidad", "root", "");
-        mesaData = new MesaData(conexion);
-        Connection cc=conexion();
-    } catch (ClassNotFoundException ex) {
+    
+        modelo=new DefaultTableModel();
+        
+        
+        mesadata = new MesaData(conexion);
+        listaMesa =(ArrayList)mesadata.obtenerMesas();
+        
+       // cargaMesas();
+        armaCabeceraTabla();
+        
+       } catch (ClassNotFoundException ex) {
         Logger.getLogger(VistaMesa.class.getName()).log(Level.SEVERE, null, ex);
-    }    
+    } 
+        jPanel1.setVisible(true);
     }
-    //METODO PARA MOSTRAR DATOS DE BBDD EN TABLA
- void mostrardatos(){
-     modelo.addColumn("id_mesa");
-     modelo.addColumn("Numero mesa");
-     modelo.addColumn("Capacidad");
-     modelo.addColumn("Estado");
-     jTable1.setModel(modelo);//imprime en la tabla
-     String []datos=new String [4];
-    try{
-            Statement st= cc.createStatement(); //enviar consultas sql al servidor
-            ResultSet rs = st.executeQuery("SELECT * FROM mesa"); //recuperar estructura de la consulta
-          while(rs.netx()){
-             datos[0]=rs.getString(0); //capturamos datos de la columna 0
-             datos[1]=rs.getString(1); 
-             datos[2]=rs.getString(2); 
-             datos[3]=rs.getString(3); 
-             modelo.addRow(datos);//agregar la fila
-          }  
-          jTable1.setModel(modelo);//mostramos en tabla jtable1
-       }catch (SQLException e){
-             Logger.getLogger(VistaMesa.class.getName()).log(Level.SEVERE, null,e);           
-      }
- }
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
@@ -76,7 +65,6 @@ private Conexion conexion;
         jtxtEstado = new javax.swing.JTextField();
         jButtonActualizar = new javax.swing.JButton();
         jButton1 = new javax.swing.JButton();
-        jButton2 = new javax.swing.JButton();
 
         setBorder(null);
 
@@ -130,40 +118,32 @@ private Conexion conexion;
 
         jButton1.setText("Volver");
 
-        jButton2.setText("Atender Mesa");
-        jButton2.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton2ActionPerformed(evt);
-            }
-        });
-
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(58, 58, 58)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addComponent(jButtonActualizar)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(168, 168, 168)
+                        .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 295, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(58, 58, 58)
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                            .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 227, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jButtonActualizar)
                             .addGroup(jPanel1Layout.createSequentialGroup()
-                                .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 106, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addGap(18, 18, 18)
-                                .addComponent(BotonConsulta)))
-                        .addGap(27, 27, 27)
-                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addGroup(jPanel1Layout.createSequentialGroup()
+                                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 227, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addGroup(jPanel1Layout.createSequentialGroup()
+                                        .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 106, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                        .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                        .addGap(18, 18, 18)
+                                        .addComponent(BotonConsulta)))
+                                .addGap(27, 27, 27)
                                 .addComponent(jButtonModificar)
                                 .addGap(43, 43, 43)
-                                .addComponent(jtxtEstado, javax.swing.GroupLayout.PREFERRED_SIZE, 105, javax.swing.GroupLayout.PREFERRED_SIZE))
-                            .addComponent(jButton2)))
-                    .addGroup(javax.swing.GroupLayout.Alignment.LEADING, jPanel1Layout.createSequentialGroup()
-                        .addGap(110, 110, 110)
-                        .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 295, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                                .addComponent(jtxtEstado, javax.swing.GroupLayout.PREFERRED_SIZE, 105, javax.swing.GroupLayout.PREFERRED_SIZE)))))
                 .addContainerGap(37, Short.MAX_VALUE))
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
@@ -188,9 +168,7 @@ private Conexion conexion;
                             .addComponent(jtxtEstado, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addComponent(jButtonModificar))
                         .addGap(18, 18, 18)
-                        .addComponent(jButtonActualizar)
-                        .addGap(8, 8, 8)
-                        .addComponent(jButton2)))
+                        .addComponent(jButtonActualizar)))
                 .addGap(62, 62, 62)
                 .addComponent(jButton1)
                 .addContainerGap(153, Short.MAX_VALUE))
@@ -211,33 +189,7 @@ private Conexion conexion;
     }// </editor-fold>//GEN-END:initComponents
 
     private void BotonConsultaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BotonConsultaActionPerformed
-     mostrardatos();
-        /*  try {
-            Class.forName("org.mariadb.jdbc.Driver");
-            Connection conexion=DriverManager.getConnection("jdbc:msql://localhost/resto", "root", "");
-             //conexion = new Conexion("jdbc:mysql://localhost/resto", "root", "");
-            Statement stm = conexion.createStatement(); //enviar consultas sql al servidor
-            ResultSet rst = stm.executeQuery("SELECT * FROM mesa"); //recuperar estructura de la consulta
-            ResultSetMetaData rsMd=rst.getMetaData(); 
-            int numeroColumnas=rsMd.getColumnCount();// numero de columnas de nuestra consulta
-            DefaultTableModel modelo =new DefaultTableModel();//crear objeto de la tabla modelo
-            this.jTable1.setModel(modelo);//asignar este objeto llamado modelo a nuestra tabla jtable1
-             for (int x=1; x<numeroColumnas; x++){   //añadir nuestros objetos de la consulta a nuestra tabla
-                 modelo.addColumn(rsMd.getColumnLabel(x));
-             }
-             while (rst.next()){ //añadir nuestros registros
-                 Object[] fila = new Object [numeroColumnas]; 
-                 for (int y=0; y<numeroColumnas; y ++){ //anadir el contenido de nuestra bbdd
-                     fila[y]=rst.getObject(y+1);
-                 }
-                 modelo.addRow(fila);
-             }
-            
-        }
-        catch(ClassNotFoundException | SQLException e){
-            e.printStackTrace();
-             System.out.println("no se pudo conectar");
-        }*/
+    
     }//GEN-LAST:event_BotonConsultaActionPerformed
 //METODO PARA TOMAR VALOR DEL ESTADO DE MESA
     private void jTable1MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jTable1MouseClicked
@@ -255,10 +207,6 @@ private Conexion conexion;
       }
     }//GEN-LAST:event_jButtonActualizarActionPerformed
 
-    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_jButton2ActionPerformed
-
     private void jComboBox1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jComboBox1ActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_jComboBox1ActionPerformed
@@ -267,7 +215,6 @@ private Conexion conexion;
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton BotonConsulta;
     private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
     private javax.swing.JButton jButtonActualizar;
     private javax.swing.JButton jButtonModificar;
     private javax.swing.JComboBox<String> jComboBox1;
@@ -279,5 +226,54 @@ private Conexion conexion;
     private javax.swing.JTextField jtxtEstado;
     // End of variables declaration//GEN-END:variables
 int filas;//variable global
+  public void cargarDatos(){
+    try {
+        borraFilasTabla();
+   
+        //Llenar filas
+   
+        MesaData cd =new MesaData(conexion);
+        Mesa seleccionado=(Mesa)jComboBox1.getSelectedItem();
+        listaMesa = (ArrayList)cd.obtenerMesas();
+        
+        for(Mesa m:listaMesa){
+        
+            modelo.addRow(new Object[]{m.getIdMesa(),m.getNumMesa(),m.getCapacidad(),m.estado()});
+        }
+    } catch (ClassNotFoundException ex) {
+        Logger.getLogger(VistaMesa.class.getName()).log(Level.SEVERE, null, ex);
+    }
+  }     
+  public void armaCabeceraTabla(){
+  
+           //Titulos de Columnas
+        ArrayList<Object> columnas=new ArrayList<Object>();
+        columnas.add("ID");
+        columnas.add("Numero");
+        columnas.add("Capacidad");
+        columnas.add("estado");
+        for(Object it:columnas){
+        
+            modelo.addColumn(it);
+        }
+        jTable1.setModel(modelo);
+  }
+
+public void borraFilasTabla(){
+
+   int a =modelo.getRowCount()-1;
+
+for(int i=a;i>=0;i--){
+   
+modelo.removeRow(i );
+}
+}
+public void cargaAlumnos(){
+    //Carga numero de mesas al ComboBox
+     for(Mesa item:listaMesa){
+            jComboBox1.addItem(item);
+    }
+
+}
 
 }
